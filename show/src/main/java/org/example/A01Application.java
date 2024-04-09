b99df73b0f0bd6deee0627ccb5bbf2694adcd77c
张召金 <zhangzhaojin@delonix.group>
2024-04-09 14:23:00 +0800

    BeanFactory & ApplicationContext & publishEvent

--- show/src/main/java/org/example/A01Application.java
+++ show/src/main/java/org/example/A01Application.java
@@ -2,16 +2,75 @@ package org.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Locale;
+import java.util.Map;
 
 @SpringBootApplication
 public class A01Application {
 
     private static final Logger log = LoggerFactory.getLogger(A01Application.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(A01Application.class, args);
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, IOException {
+        ConfigurableApplicationContext context = SpringApplication.run(A01Application.class, args);
+        /**
+         * 1. 到底什么是BeanFactory
+         *  - 它是 ApplicationContext 的父接口
+         *  - 它才是 Spring 的核心容器, 主要的 ApplicationContext 实现都【组合】了它的功能
+         */
+        System.out.println(context);
+//        context.getBean("aaa");
+
+        /**
+         * 2. BeanFactory 能干点啥
+         *  - 表面上只有 getBean
+         *  - 实际上控制反转、基本的依赖注入、直至 Bean 的生命周期的各种功能, 都由它的实现类提供
+         */
+        Field singletonObjects = DefaultSingletonBeanRegistry.class.getDeclaredField("singletonObjects");
+        singletonObjects.setAccessible(true);
+        ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
+        Map<String, Object> map = (Map<String, Object>) singletonObjects.get(beanFactory);
+//        map.forEach((k,v)->{
+//            System.out.println(k + "=" + v);
+//        });
+        map.entrySet().stream().filter(e -> e.getKey().startsWith("component"))
+                .forEach(e -> {
+                    System.out.println(e.getKey() + "=" + e.getValue());
+                });
+
+        /**
+         * 3. ApplicationContext
+         */
+        System.out.println(context.getMessage("hi", null, Locale.CHINA));
+        System.out.println(context.getMessage("hi", null, Locale.ENGLISH));
+        System.out.println(context.getMessage("hi", null, Locale.JAPANESE));
+
+        Resource[] resources = context.getResources("classpath:application.properties");
+        for (Resource resource : resources) {
+            System.out.println(resource.getURL());
+        }
+
+        context.publishEvent(new UserRegisteredEvent(context));
+//        context.getBean(Component1.class).register();
+        System.out.println(context.getEnvironment().getProperty("java_home"));
+        System.out.println(context.getEnvironment().getProperty("server.port"));
+
+        /**
+         * 4. 学到了什么
+         *  a. BeanFactory 与 ApplicationContext 并不仅仅是简单接口继承的关系, ApplicationContext 组合并扩展了 BeanFactory 的功能
+         *  b. 又新学一种代码之间解耦途径
+         * 联系: 完成用户注册与发送短信之间的解耦, 用事件方式、和 AOP 方式分别实现
+         */
+
+
 
 
     }
